4a00b55957f90900cafbeef469a7474ff08392d1
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-05-24 15:55:30 -0300

    feat: create general button component

---
+++ src/app/_components/header/ButtonDownloadCV.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+interface ButtonProps {
+    children: React.ReactNode;
+}
+
+function ButtonDownloadCV({ children }: ButtonProps) {
+    return (
+        <div className='flex items-center h-full'>
+            <div className='relative bg-gradient-to-t from-[#3b82f6] to-white rounded-lg'>
+                <button className="
+                    relative
+                    flex flex-row gap-1 
+                    items-center justify-center
+                    border-2 border-transparent
+                    bg-[#2B2C4F]
+                    px-2 py-1 m-[1px]
+                    rounded-lg
+                    hover:bg-[#353660]
+                    font-poppins font-medium text-sm
+                    transition-all duration-300 ease-linear">
+                    {children}
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ButtonDownloadCV
