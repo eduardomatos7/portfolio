92a3924e34174ac5d4a86646bbe443175c704cd9
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-05-27 14:23:15 -0300

    feat: creates file with project data

---
+++ src/app/_utils/projects.ts
@@ -0,0 +1,29 @@
+
+export const medicAppProject = {
+    technologies: [
+    { id: "1", tech: "/projects/reactnativeIcon.png", alt: "React Native", width: 20 },
+    { id: "4", tech: "/techs/typescript.png", alt: "Typescript", width: 20 },
+    { id: "3", tech: "/projects/fastapiNameIcon.png", alt: "FastAPI", width: 50 },
+    { id: "2", tech: "/projects/awsIcon.png", alt: "AWS", width: 20 },
+    { id: "5", tech: "/techs/postgres.png", alt: "Postgres.png", width: 20 },
+],
+    title: "MedicApp",
+    description: "Aplicativo para verificar interações entre medicamentos.",
+    srcImage: "/projects/cover/medicappImage.png",
+    deploy: "#",
+    github: "https://github.com/RicardoMorato/MedicApp"
+}
+export const KanbanProject = {
+    technologies: [
+    { id: "1", tech: "/techs/logos--react.png", alt: "React", width: 20 },
+    { id: "2", tech: "/techs/javascript.png", alt: "Javascript", width: 20 },
+    { id: "3", tech: "/techs/css3.png", alt: "Css", width: 50 },
+    { id: "4", tech: "/projects/radixui.png", alt: "RadixUi", width: 20 },
+    { id: "5", tech: "/techs/next.js.png", alt: "NextJs", width: 20 },
+],
+    title: "Quadro Kanban",
+    description: "Quadro kanban para gerenciar tarefas.",
+    srcImage: "/projects/cover/KanbanTODO.png",
+    deploy: "https://board-kanban-sigma.vercel.app/",
+    github: "https://github.com/eduardomatos7/To-do-List-Kanban"
+}
