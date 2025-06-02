2afd786d87dca5010f1fb4312a3f148d48e97859
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-06-02 12:43:05 -0300

    fix: rename image directoy

--- src/app/_utils/education.ts
+++ src/app/_utils/education.ts
@@ -25,7 +25,7 @@ export const onebitcodeInfo = {
 };
 
 export const ifInfo = {
-    logo: "/assets/images/education/IfLogo.png",
+    logo: "/assets/images/education/ifLogo.png",
     alt: "LogoIf",
     titleAcademic: "Curso Técnico em Informática",
     educationalInstitution: "Instituto Federal Baiano - Campus Lapa",
