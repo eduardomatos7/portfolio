fb9aa89eb6384c2a46ba4b5fb018e5b8278e0390
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-06-02 00:14:08 -0300

    feat: update readme

--- src/app/_components/HomePage.tsx
+++ src/app/_components/HomePage.tsx
@@ -10,16 +10,15 @@ function HomePage() {
         <>
             <HomeSection />
             <section className="mt-16 md:mt-10">
-
                 <Carousel />
             </section>
-            <section id="projects" className="mt-16 md:mt-32 pb-10 min-h-screen">
+            <section id="projects" className="mt-16 md:mt-32 pb-10 min-h-screen scroll-mt-16">
                 <Projects />
             </section>
-            <section id="skills" className=" mt-16 md:mt-32 pb-10 min-h-screen ">
+            <section id="skills" className=" mt-16 md:mt-32 pb-10 min-h-screen scroll-mt-16">
                 <SkillsPage />
             </section >
-            <section id="education" className=" mt-16 md:mt-32 min-h-screen">
+            <section id="education" className=" mt-16 md:mt-32 min-h-screen scroll-mt-6">
                 <EducationPage />
             </section >
         </>
